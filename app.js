db3f9814b239aadfa7d6540116affcdfd94ba769
Josh Jones <38821732+sonata7245@users.noreply.github.com>
2022-09-22 14:50:46 -0700

    draw grid in container

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const row = document.querySelector('.row')
 
-for (let i = 0; i <= 10; i++){
-    box = document.createElement('div')
-    box.classList.add('grid')
-    row.append(box);
+let num = 10;
+
+const container = document.querySelector('#gridContainer')
+
+
+for (let i = 0; i <= num; i++) {
+   row = document.createElement('div')
+   row.classList.add('row')
+    for (let j = 0; j <= num; j++) {
+        box = document.createElement('div')
+        box.classList.add('grid')
+        row.append(box);
+    }
+    container.append(row);
 }
