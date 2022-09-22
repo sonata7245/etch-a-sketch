e616c2dc4dc3503545c5c80f47dab7ed2dd00468
Josh Jones <38821732+sonata7245@users.noreply.github.com>
2022-09-22 15:19:01 -0700

    add hover color

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 
-let num = 10;
-
 const container = document.querySelector('#gridContainer')
 
-
-for (let i = 0; i <= num; i++) {
-   row = document.createElement('div')
-   row.classList.add('row')
-    for (let j = 0; j <= num; j++) {
-        box = document.createElement('div')
-        box.classList.add('grid')
-        row.append(box);
+function drawGrid(num) {
+    for (let i = 0; i <= num; i++) {
+        row = document.createElement('div')
+        row.classList.add('row')
+        for (let j = 0; j <= num; j++) {
+            box = document.createElement('div')
+            box.classList.add('grid')
+            row.append(box);
+        }
+        container.append(row);
     }
-    container.append(row);
+    [document.querySelector('.grid')].forEach(item => {
+        item.addEventListener('hover', event =>{
+            box.style.color = red;
+        })
+    })
 }
+
+drawGrid(10);
+
