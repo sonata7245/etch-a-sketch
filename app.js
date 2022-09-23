25a2b262019112b8e215cddd15cc50365573d05c
sonata7245 <sonata7245@gmail.com>
2022-09-22 22:24:41 -0700

    Add Rainbow, color picker, reset, eraser, grid.

--- app.js
+++ app.js
@@ -1,28 +1,84 @@
 
+
 const container = document.querySelector('#gridContainer')
-container.addEventListener('mouseover', event => {
-    console.log(event.target)
-    box = event.target;
-    box.style.background = 'red';
-  })
+const resetButton = document.querySelector('#reset');
+const gridSizeSelector = document.querySelector('#gridSize');
+const rainbowButton = document.querySelector('#rainbow');
+const colorPicker = document.querySelector('#colorPicker');
+const eraserButton = document.querySelector('#eraser');
+
+let cursorColor = '#000000';
+let activeRainbow = false;
+
+drawGrid(16);
+
+function getRandomInt() {
+    return Math.floor(Math.random() * 256);
+  }
+
+function getRainbow (){
+    let r = getRandomInt()
+    let g = getRandomInt()
+    let b = getRandomInt()
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
 function drawGrid(num) {
-    for (let i = 0; i <= num; i++) {
+    removeAllChildNodes(container);
+    for (let i = 1; i <= num; i++) {
         row = document.createElement('div')
         row.classList.add('row')
-        for (let j = 0; j <= num; j++) {
+        for (let j = 1; j <= num; j++) {
             box = document.createElement('div')
             box.classList.add('grid')
             row.append(box);
         }
         container.append(row);
     }
-    // [document.querySelector('.grid')].forEach(item => {
-    //     item.addEventListener('hover', event =>{
-    //         item.style.background = red;
-    //     })
-    // })
 }
 
-drawGrid(10);
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
+
+container.addEventListener('mouseover', event => {
+    box = event.target;
+    if (activeRainbow){
+    box.style.background = getRainbow();}
+    else {
+    box.style.background = cursorColor;
+    }
+
+})
+
+resetButton.addEventListener('click', () => {
+    cursorColor = '#000000';
+    drawGrid(16);
+    colorPicker.value = '#000000';
+})
+
+
+gridSizeSelector.addEventListener('change', function (e) {
+    let num = this.value
+    drawGrid(num);
+} )
+
+rainbowButton.addEventListener('click', (e) => {
+    return activeRainbow = true
+} )
+colorPicker.addEventListener('change', function (e) {
+    activeRainbow = false
+    cursorColor = (this.value);
+} )
+eraserButton.addEventListener('click', (e) => {
+    activeRainbow = false
+    cursorColor = '#FFFFFF';
+} )
+
+
 
